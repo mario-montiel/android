f986ea27933cbebe396d9a9f89ff54697cdb4f78
mario-montiel <mario159_@hotmail.com>
2019-03-18 22:34:30 -0600

    TalleresUTT
    pruebon

--- resources/views/TalleresUTT/Talleres/mostrarTalleres.blade.php
+++ resources/views/TalleresUTT/Talleres/mostrarTalleres.blade.php
@@ -62,9 +62,7 @@
         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0" action="/filtroresultado" method="GET">
       <input name="buscador" id="buscador" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    </form>
   </div>
 </nav>
 
