68ea6b80988c88f3fea247899cfd8d397314f9a9
mario-montiel <mario159_@hotmail.com>
2019-04-08 23:17:45 -0500

    talleresUTT
    pruebon

--- resources/views/TalleresUTT/Alumnos/mostrarAlumnos.blade.php
+++ resources/views/TalleresUTT/Alumnos/mostrarAlumnos.blade.php
@@ -13,7 +13,7 @@
 <br>
 @if( count($alumnos)>0)
 <div class="container-fluid">
-    <table class="table table-responsive" style="margin-left:5%;">
+    <table class="table table-responsive" style="margin-left:7%;">
         <thead>
             <tr>
                 <th>Usuario</th>
