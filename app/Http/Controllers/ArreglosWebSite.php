3d0eb75187a183c367a2ca9b25b3be13331ed263
mario-montiel <mario159_@hotmail.com>
2019-04-11 20:28:05 -0500

    talleresUTT
    pruebon

--- app/Http/Controllers/ArreglosWebSite.php
+++ app/Http/Controllers/ArreglosWebSite.php
@@ -47,7 +47,7 @@ class ArreglosWebSite extends Controller
     }
 
     function arregloJohnnyWuW(Request $request){
-        $usuario = Usuario::where("usuario", "=", $request->usuario)->get();
+        $usuario = Login::where("usuario", "=", $request->usuario)->get();
         $user = Session::get('user', $usuario);
         if($user){
             return "hola";
