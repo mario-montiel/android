967d581161a1342ec6631d647eb0aa4b824c23e4
mario-montiel <mario159_@hotmail.com>
2019-04-12 10:08:57 -0500

    talleresUTT
    pruebon

--- resources/views/TalleresUTT/Home/home.blade.php
+++ resources/views/TalleresUTT/Home/home.blade.php
@@ -240,15 +240,6 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto" style="margin-left:50px;">
       <li class="nav-item active">
-        <a  class="nav-link" href="/mostrartalleres">Talleres <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a  class="nav-link" href="/mostraralumnos">Alumnos <span class="sr-only">(current)</span></a>
-      </li>
-	  <li class="nav-item active">
-        <a  class="nav-link" href="/eventos">Eventos <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
         <a  class="nav-link" href="/eventosasignados">Asignar Evento <span class="sr-only">(current)</span></a>
       </li>
     </ul>
