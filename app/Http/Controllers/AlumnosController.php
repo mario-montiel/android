800a1b77bc1ce242a519879286f0dedc78c94ce4
mario-montiel <mario159_@hotmail.com>
2019-04-20 13:48:37 -0500

    talleresUTT
    pruebon

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -55,7 +55,7 @@ class AlumnosController extends Controller
 
     function buscatesta(Request $request){
         $alumnos = DB::table('usuarios')
-        ->select('personas.matricula', 'personas.nombre', 'personas.carreras_id_carrera', 'personas.cuatrimestre_id_cuatrimestre', 'talleres.taller', 'solicitudes.horas_servicio_social', 'usuarios.created_at', 'usuarios.updated_at')
+        ->select('personas.matricula', 'personas.nombre', 'carreras.nombre', 'cuatrimestre.cuatrimestre', 'talleres.taller', 'solicitudes.horas_servicio_social', 'usuarios.created_at', 'usuarios.updated_at')
         ->rightjoin('personas', 'personas.id_persona', '=', 'usuarios.personas_id_persona')
         ->leftjoin('solicitudes','solicitudes.personas_id_persona', '=', 'personas.id_persona')
         ->rightjoin('talleres','talleres.id_taller', '=', 'solicitudes.tallleres_id_taller')
