a1708172fdf14e2313cc3c1ec9adf5dc4b85a677
mario-montiel <mario159_@hotmail.com>
2019-04-29 19:42:11 -0500

    talleresUTT
    pruebon

--- app/Http/Controllers/ArreglosWebSite.php
+++ app/Http/Controllers/ArreglosWebSite.php
@@ -67,19 +67,21 @@ class ArreglosWebSite extends Controller
     }
 
     function solicitud($usuario, $taller){
-        $obj="";
+        /*$obj="";
         $id= DB::table('usuarios')
         ->join('personas', 'usuarios.personas_id_persona', '=', 'personas.id_persona')
         ->where('usuarios.usuario', $request->usuario)->get();
         $array = json_decode($id, true);
         foreach ($array as $key => $con) {
             $obj=$con['personas_id_persona'];
-        }
+        }*/
+
+        $id = Session::get('usuario')->personas_id_persona;
         
         $solicitud = new Solicitud();
         $solicitud->tallleres_id_taller = $request->taller;
         $solicitud->horas_servicio_social = 0;
-        $solicitud->personas_id_persona = $obj;
+        $solicitud->personas_id_persona = $id;
         $solicitud->save();
     }
 
