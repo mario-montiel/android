70d5abc24ddf32b893eb21e9e6a9e99e3abc5797
mario-montiel <mario159_@hotmail.com>
2019-05-06 13:18:53 -0500

    talleresUTT
    pruebon

--- app/Http/Controllers/ArreglosWebSite.php
+++ app/Http/Controllers/ArreglosWebSite.php
@@ -70,8 +70,10 @@ class ArreglosWebSite extends Controller
 
         $vato = DB::table('usuarios')
             ->join('personas', 'personas.id_persona', 'usuarios.personas_id_persona')
+            ->join('tipos_personas', 'tipos_personas.id_tipo_persona', '=', 'personas.tipos_personas_id_tipo_persona')
             ->where('usuario', $request->usuario)
             ->where('personas.tipos_personas_id_tipo_persona', 2)
+            ->select('usuarios.password', 'tipos_personas.tipo')
             ->first();
 
         if($vato){
@@ -82,7 +84,7 @@ class ArreglosWebSite extends Controller
             $confirmarpass = $usuario->password;
             $user = $usuario->usuario;
 
-            if (Hash::check($pass, $confirmarpass) && $user == $confirmar) {
+            if (Hash::check($confirmarpass, $pass) && $user == $confirmar) {
                 return true;
             }
         }
